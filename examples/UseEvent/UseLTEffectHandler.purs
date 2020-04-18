9f83809492480464f0a0a73d167dfda66911beb7
Jordan Martinez <jordanalex.martinez@gmail.com>
2020-04-17 19:37:08 -0700

    Refactor 'useEvent' with unsubscribe capability into more hook-like code

--- examples/UseEvent/UseLTEffectHandler.purs
+++ examples/UseEvent/UseLTEffectHandler.purs
@@ -26,43 +26,28 @@ component = Hooks.component \_ -> Hooks.do
     pushValueAndTriggerCallback :: Int -> HookM _ _ Aff Unit
     pushValueAndTriggerCallback value = do
       mbCallback <- liftEffect $ Ref.read callbackRef
+      let
+        setupUnsubscribeCallback = \unsubscribeCode -> do
+          mbUnsubscribe <- liftEffect $ Ref.read unsubscribeRef
+          case mbUnsubscribe of
+            Nothing -> do
+              -- This is the first time we're running this, so
+              -- write the unsubscribe code to a Ref,
+              -- so that we can run it later
+              liftEffect $ Ref.write (Just unsubscribeCode) unsubscribeRef
+            _ -> do
+              -- no need to store unsubscriber because
+              -- 1. it's already been stored (i.e. this is the 2+ run)
+              -- 2. we didn't setup any resources that need to be cleaned up later
+              --     (i.e. this is purely an event handler)
+              pure unit
       for_ mbCallback \callback -> do
-        callback value
+        callback setupUnsubscribeCallback value
 
     setCallback callback =
       liftEffect $ Ref.write callback callbackRef
 
-  -- at some point in code, we trigger the callback via
-  -- `pushValueAndTriggerCallback`
-
-  -- Here, we set a callback that will handle the events emitted.
-  -- We chose to use the `useTickEffect` version, but we could
-  -- could have used the `useLifecycleEffect`
-  Hooks.captures { state } useTickEffect do
-    setCallback $ Just \i -> do
-      -- here, we handle the event emitted
-      liftEffect $ log $ "New value is: " <> show i
-
-      -- here, we can set up some resources and do various things
-      -- (code is not shown; use your imagination)
-
-      -- here, we store the code necessary for unsubscribing
-      let unsubscribeCode = pure unit -- unsubscribe
-      mbUnsubscribe <- liftEffect $ Ref.read unsubscribeRef
-      case mbUnsubscribe of
-        Nothing -> do
-          -- This is the first time we're running this, so
-          -- write the unsubscribe code to a Ref,
-          -- so that we can run it later
-          liftEffect $ Ref.write (Just unsubscribeCode) unsubscribeRef
-        _ -> do
-          -- no need to store unsubscriber because
-          -- 1. it's already been stored (i.e. this is the 2+ run)
-          -- 2. we didn't setup any resources that need to be cleaned up later
-          --     (i.e. this is purely an event handler)
-          pure unit
-
-    pure $ Just do
+    unsubscribe = do
       -- Here, we read the Ref to get back the unsubscribe code,
       -- run it, and then set the `Ref` back to `Nothing`,
       -- so that future subscribes will work.
@@ -73,5 +58,26 @@ component = Hooks.component \_ -> Hooks.do
           liftEffect $ Ref.write Nothing unsubscribeRef
         _ -> do
           pure unit
+
+  -- at some point in code, we trigger the callback via
+  -- `pushValueAndTriggerCallback`
+
+  -- Here, we set a callback that will handle the events emitted.
+  -- We chose to use the `useTickEffect` version, but we could
+  -- could have used the `useLifecycleEffect`
+  Hooks.captures { state } useTickEffect do
+    setCallback $ Just \unsubscribeCallback i -> do
+      -- here, we handle the event emitted
+      liftEffect $ log $ "New value is: " <> show i
+
+      -- here, we can set up some resources and do various things
+
+      -- here, we store the code necessary for unsubscribing
+      unsubscribeCallback do
+        let unsubscribe' = pure unit -- unsubscribe
+        unsubscribe'
+
+    pure $ Just do
+      unsubscribe
 
   Hooks.pure $ HH.text "example"
