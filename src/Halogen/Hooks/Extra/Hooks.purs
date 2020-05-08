9d5a34dd110b69e7f13954917f784bc705ee89c8
Jordan Martinez <jordanalex.martinez@gmail.com>
2020-05-07 19:23:58 -0700

    Add a single module that reexports all hooks

---
+++ src/Halogen/Hooks/Extra/Hooks.purs
@@ -0,0 +1,9 @@
+-- | Reexports all Hooks defined in this repository.
+module Halogen.Hooks.Extra.Hooks
+  ( module Exports
+
+  ) where
+
+import Halogen.Hooks.Extra.Hooks.UseDebouncer (useDebouncer) as Exports
+import Halogen.Hooks.Extra.Hooks.UseGet (useGet) as Exports
+import Halogen.Hooks.Extra.Hooks.UseEvent (useEvent) as Exports
