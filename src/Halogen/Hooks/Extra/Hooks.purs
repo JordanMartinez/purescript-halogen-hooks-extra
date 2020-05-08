a92379fe2983eaa7ea3215f4b7bad2264d2efe04
Jordan Martinez <jordanalex.martinez@gmail.com>
2020-05-07 21:13:22 -0700

    Also export types of Hooks besides their implementation

--- src/Halogen/Hooks/Extra/Hooks.purs
+++ src/Halogen/Hooks/Extra/Hooks.purs
@@ -1,9 +1,10 @@
 -- | Reexports all Hooks defined in this repository.
 module Halogen.Hooks.Extra.Hooks
-  ( module Exports
-
+  ( module Halogen.Hooks.Extra.Hooks.UseDebouncer
+  , module Halogen.Hooks.Extra.Hooks.UseGet
+  , module Halogen.Hooks.Extra.Hooks.UseEvent
   ) where
 
-import Halogen.Hooks.Extra.Hooks.UseDebouncer (useDebouncer) as Exports
-import Halogen.Hooks.Extra.Hooks.UseGet (useGet) as Exports
-import Halogen.Hooks.Extra.Hooks.UseEvent (useEvent) as Exports
+import Halogen.Hooks.Extra.Hooks.UseDebouncer (UseDebouncer(..), useDebouncer)
+import Halogen.Hooks.Extra.Hooks.UseGet (UseGet(..), useGet)
+import Halogen.Hooks.Extra.Hooks.UseEvent (UseEvent(..), useEvent)
