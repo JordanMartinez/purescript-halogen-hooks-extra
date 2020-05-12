116b4922fe3dfcfe45bead6be0ab4fcfaca9a083
Jordan Martinez <jordanalex.martinez@gmail.com>
2020-05-11 18:57:41 -0700

    Refactor exports to make adding new hooks easier

--- src/Halogen/Hooks/Extra/Hooks.purs
+++ src/Halogen/Hooks/Extra/Hooks.purs
@@ -1,12 +1,9 @@
 -- | Reexports all Hooks defined in this repository.
 module Halogen.Hooks.Extra.Hooks
-  ( module Halogen.Hooks.Extra.Hooks.UseDebouncer
-  , module Halogen.Hooks.Extra.Hooks.UseGet
-  , module Halogen.Hooks.Extra.Hooks.UseEvent
-  , module Halogen.Hooks.Extra.Hooks.UseThrottle
+  ( module Exports
   ) where
 
-import Halogen.Hooks.Extra.Hooks.UseDebouncer (UseDebouncer, useDebouncer)
-import Halogen.Hooks.Extra.Hooks.UseGet (UseGet, useGet)
-import Halogen.Hooks.Extra.Hooks.UseEvent (UseEvent, UseEventApi, useEvent)
-import Halogen.Hooks.Extra.Hooks.UseThrottle (UseThrottle, useThrottle)
+import Halogen.Hooks.Extra.Hooks.UseDebouncer (UseDebouncer, useDebouncer) as Exports
+import Halogen.Hooks.Extra.Hooks.UseGet (UseGet, useGet) as Exports
+import Halogen.Hooks.Extra.Hooks.UseEvent (UseEvent, UseEventApi, useEvent) as Exports
+import Halogen.Hooks.Extra.Hooks.UseThrottle (UseThrottle, useThrottle) as Exports
