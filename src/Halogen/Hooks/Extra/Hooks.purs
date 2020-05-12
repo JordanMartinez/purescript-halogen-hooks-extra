2fc9977a01a1e53c5154471f40ca3a5924d89756
Jordan Martinez <jordanalex.martinez@gmail.com>
2020-05-11 18:57:02 -0700

    Add `useThrottle` hook to mass-export module

--- src/Halogen/Hooks/Extra/Hooks.purs
+++ src/Halogen/Hooks/Extra/Hooks.purs
@@ -3,8 +3,10 @@ module Halogen.Hooks.Extra.Hooks
   ( module Halogen.Hooks.Extra.Hooks.UseDebouncer
   , module Halogen.Hooks.Extra.Hooks.UseGet
   , module Halogen.Hooks.Extra.Hooks.UseEvent
+  , module Halogen.Hooks.Extra.Hooks.UseThrottle
   ) where
 
 import Halogen.Hooks.Extra.Hooks.UseDebouncer (UseDebouncer, useDebouncer)
 import Halogen.Hooks.Extra.Hooks.UseGet (UseGet, useGet)
 import Halogen.Hooks.Extra.Hooks.UseEvent (UseEvent, UseEventApi, useEvent)
+import Halogen.Hooks.Extra.Hooks.UseThrottle (UseThrottle, useThrottle)
