dc19be0091fa58dac86ac6283e6823afc7451a4c
Miles Frain <miles.frain@colorado.edu>
2020-07-08 12:57:04 -0700

    Add usePutState, usePutState, and comments

--- src/Halogen/Hooks/Extra/Hooks.purs
+++ src/Halogen/Hooks/Extra/Hooks.purs
@@ -6,5 +6,5 @@ module Halogen.Hooks.Extra.Hooks
 import Halogen.Hooks.Extra.Hooks.UseDebouncer (UseDebouncer, useDebouncer) as Exports
 import Halogen.Hooks.Extra.Hooks.UseEvent (UseEvent, UseEventApi, useEvent) as Exports
 import Halogen.Hooks.Extra.Hooks.UseGet (UseGet, useGet) as Exports
-import Halogen.Hooks.Extra.Hooks.UseStateFn (UseStateFn, useStateFn, useModifyState) as Exports
+import Halogen.Hooks.Extra.Hooks.UseStateFn (UseStateFn, useStateFn, useModifyState_, useModifyState, usePutState) as Exports
 import Halogen.Hooks.Extra.Hooks.UseThrottle (UseThrottle, useThrottle) as Exports
